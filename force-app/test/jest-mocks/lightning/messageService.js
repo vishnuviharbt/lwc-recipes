45910e1dbb72d3c33d38e6e5dfdd4b735968180d
Peter Chittum <pchittum@salesforce.com>
2020-05-28 10:30:04 +0100

    added publisher test and subscriber test started

---
+++ force-app/test/jest-mocks/lightning/messageService.js
@@ -0,0 +1,9 @@
+/*
+ * Stub for Lightning Message Service (lightning/messageService)
+ */
+export const createMessageContext = () => {};
+export const releaseMessageContext = () => {};
+export const publish = () => {};
+export const subscribe = () => {};
+export const unsubscribe = () => {};
+export const APPLICATION_SCOPE = '';
