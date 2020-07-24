996ca53104387fba2237053d1c1667dae8694a7b
Peter Chittum <pchittum@salesforce.com>
2020-07-24 13:37:03 +0100

    checkpoint for failing tests

--- force-app/main/default/lwc/miscPermissionBasedUI/__tests__/miscPermissionBasedUI.test.js
+++ force-app/main/default/lwc/miscPermissionBasedUI/__tests__/miscPermissionBasedUI.test.js
@@ -7,12 +7,21 @@ describe('c-misc-permission-based-u-i', () => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+        jest.clearAllMocks();
     });
 
     // This is a placeholder test that simulates the "assigned permission" behavior
     // of this component. Currently exploring correct way to mock the permission
     // values of true and undefined that are experienced at runtime.
-    it('displays the correct UI when custom permission is truthy', () => {
+    it('displays the correct UI when custom permission is true', () => {
+        jest.mock(
+            '@salesforce/customPermission/accessRestrictedUIPermission',
+            () => {
+                return { default: true };
+            },
+            { virtual: true }
+        );
+
         const element = createElement('c-misc-permission-based-u-i', {
             is: MiscPermissionBasedUI
         });
@@ -20,5 +29,23 @@ describe('c-misc-permission-based-u-i', () => {
 
         const pEl = element.shadowRoot.querySelector('p');
         expect(pEl.textContent).toBe('The permission set is assigned');
+    });
+
+    it('displays the correct UI when custom permission is undefined', () => {
+        jest.mock(
+            '@salesforce/customPermission/accessRestrictedUIPermission',
+            () => {
+                return { default: undefined };
+            },
+            { virtual: true }
+        );
+
+        const element = createElement('c-misc-permission-based-u-i', {
+            is: MiscPermissionBasedUI
+        });
+        document.body.appendChild(element);
+
+        const pEl = element.shadowRoot.querySelector('p');
+        expect(pEl.textContent).toBe('The permission set is not assigned');
     });
 });
